369a42ae0a6876cf25e714c2e93800f6f813cf93
Ahmet Goch <ahmetgoch@yandex.ru>
2021-07-09 10:23:50 +0200

    add Trips

--- backend/config/routing.py
+++ backend/config/routing.py
@@ -1,7 +1,13 @@
 from django.core.asgi import get_asgi_application
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-from channels.routing import ProtocolTypeRouter
+from trips.consumers import TaxiConsumer
+
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
+    'websocket': URLRouter([
+        path('config/', TaxiConsumer.as_asgi()),
+    ]),
 })
