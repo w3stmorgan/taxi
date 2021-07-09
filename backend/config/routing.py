605360922337b59eab17763a7782fb0fed3da5d3
Ahmet Goch <ahmetgoch@yandex.ru>
2021-07-09 10:39:53 +0200

    configure websockets

--- backend/config/routing.py
+++ backend/config/routing.py
@@ -1,13 +1,17 @@
 from django.core.asgi import get_asgi_application
 from django.urls import path
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 
+from config.middleware import TokenAuthMiddlewareStack
 from trips.consumers import TaxiConsumer
 
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': URLRouter([
-        path('config/', TaxiConsumer.as_asgi()),
-    ]),
+    'websocket': TokenAuthMiddlewareStack( 
+        URLRouter([
+            path('config/', TaxiConsumer.as_asgi()),
+        ])
+    ),
 })
